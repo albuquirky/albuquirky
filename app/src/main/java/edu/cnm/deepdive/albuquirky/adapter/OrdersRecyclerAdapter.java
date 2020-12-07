7c1d00148a2c0ae05fc419266c027bfb5dcbaaea
Ricky Garcia <rickyg08@msn.com>
2020-12-07 16:07:41 -0700

    Working on adapter

--- app/src/main/java/edu/cnm/deepdive/albuquirky/adapter/OrdersRecyclerAdapter.java
+++ app/src/main/java/edu/cnm/deepdive/albuquirky/adapter/OrdersRecyclerAdapter.java
@@ -1,15 +1,48 @@
 package edu.cnm.deepdive.albuquirky.adapter;
 
+import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import edu.cnm.deepdive.albuquirky.adapter.OrdersRecyclerAdapter.Holder;
 import edu.cnm.deepdive.albuquirky.databinding.OrdersBinding;
+import edu.cnm.deepdive.albuquirky.model.Order;
+import java.util.ArrayList;
 import java.util.List;
+import org.jetbrains.annotations.NotNull;
 
-public class OrdersRecyclerAdapter {
+public class OrdersRecyclerAdapter extends RecyclerView.Adapter<Holder> {
 
-//  private final List<Orders> orders;
+  private final Context context;
+  private final List<Order> orders;
   private final LayoutInflater inflater;
+
+  public OrdersRecyclerAdapter(Context context) {
+    this.context = context;
+    orders = new ArrayList<>();
+    inflater = LayoutInflater.from(context);
+  }
+
+  public List<Order> getOrders() {
+    return orders;
+  }
+
+  @NonNull
+  @Override
+  public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    return null;
+  }
+
+  @Override
+  public void onBindViewHolder(@NonNull Holder holder, int position) {
+    holder.bind(position);
+  }
+
+  @Override
+  public int getItemCount() {
+    return orders.size();
+  }
 
   public class Holder extends RecyclerView.ViewHolder {
 
@@ -21,7 +54,8 @@ public class OrdersRecyclerAdapter {
     }
 
     private void bind(int position){
-      ;
+      Order order = orders.get(position);
+      binding.
     }
   }
 }
