7c17f88cd0c84dd69e7f2e3b667fa70805df2553
Ricky Garcia <rickyg08@msn.com>
2020-12-09 14:55:02 -0700

    Added orders to fragment_orders.

--- app/src/main/java/edu/cnm/deepdive/albuquirky/adapter/OrdersRecyclerAdapter.java
+++ app/src/main/java/edu/cnm/deepdive/albuquirky/adapter/OrdersRecyclerAdapter.java
@@ -9,23 +9,35 @@ import edu.cnm.deepdive.albuquirky.adapter.OrdersRecyclerAdapter.Holder;
 import edu.cnm.deepdive.albuquirky.databinding.OrdersBinding;
 import edu.cnm.deepdive.albuquirky.model.Order;
 import edu.cnm.deepdive.albuquirky.model.Product;
+import edu.cnm.deepdive.albuquirky.model.ProductOnOrder;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ *
+ */
 public class OrdersRecyclerAdapter extends RecyclerView.Adapter<Holder> {
 
   private final Context context;
-  private final List<Order> orders;
   private final LayoutInflater inflater;
+  private final List<ProductOnOrder> orders;
 //  private final Image image;
 
+  /**
+   *
+   * @param context
+   */
   public OrdersRecyclerAdapter(Context context) {
     this.context = context;
-    orders = new ArrayList<>();
     inflater = LayoutInflater.from(context);
+    orders = new ArrayList<>();
   }
 
-  public List<Order> getOrders() {
+  /**
+   *
+   * @return
+   */
+  public List<ProductOnOrder> getOrders() {
     return orders;
   }
 
@@ -44,26 +56,34 @@ public class OrdersRecyclerAdapter extends RecyclerView.Adapter<Holder> {
   @Override
   public int getItemCount() {
     return orders.size();
-  }
+}
 
+  /**
+   *
+   */
   class Holder extends RecyclerView.ViewHolder {
 
-    private final OrdersBinding binding;
+  private final OrdersBinding binding;
 
-    public Holder(@NonNull OrdersBinding binding) {
-      super(binding.getRoot());
-      this.binding = binding;
-    }
+    /**
+     * 
+     * @param binding
+     */
+  public Holder(@NonNull OrdersBinding binding) {
+    super(binding.getRoot());
+    this.binding = binding;
+  }
 
-    private void bind(int position) {
-      Order order = orders.get(position);
+  private void bind(int position) {
+    ProductOnOrder order = orders.get(position);
+//    binding.itemNameOrderId.setText(order.getProduct());
+    binding.itemCost.setText(order.getUnitPrice());
 //      Product
-//      binding.itemCost.setText(getItemCost);
 //      binding.itemNameOrderId.setText(Product..);
 //      binding.itemNameOrderId.setText(order.getOrderId);
 //      binding.orderDatePlaced.setText(order.getPlacedDate());
 //      binding.itemCost.setText();
 //      binding.productOrderImage.;
-    }
   }
+}
 }
--- app/src/main/java/edu/cnm/deepdive/albuquirky/controller/ui/orders/OrdersFragment.java
+++ app/src/main/java/edu/cnm/deepdive/albuquirky/controller/ui/orders/OrdersFragment.java
@@ -5,17 +5,69 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.ViewModelProvider;
+import edu.cnm.deepdive.albuquirky.adapter.OrdersRecyclerAdapter;
+import edu.cnm.deepdive.albuquirky.controller.MainViewModel;
 import edu.cnm.deepdive.albuquirky.databinding.FragmentOrdersBinding;
+import edu.cnm.deepdive.albuquirky.model.Order;
+import edu.cnm.deepdive.albuquirky.model.ProductOnOrder;
+import java.util.List;
 
+/**
+ *
+ */
 public class OrdersFragment extends Fragment {
 
   private FragmentOrdersBinding binding;
+  private OrdersRecyclerAdapter adapter;
+  private OrdersViewModel ordersViewModel;
 
+  /**
+   *
+   * @param inflater
+   * @param container
+   * @param savedInstanceState
+   * @return
+   */
   public View onCreateView(@NonNull LayoutInflater inflater,
       ViewGroup container, Bundle savedInstanceState) {
     binding = FragmentOrdersBinding.inflate(inflater, container, false);
     return binding.getRoot();
   }
+
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    setupViewModel();
+  }
+
+   private void setupViewModel() {
+     FragmentActivity activity = getActivity();
+     adapter = new OrdersRecyclerAdapter(activity);
+//     binding.ordersList.setAdapter(adapter);
+     ordersViewModel = new ViewModelProvider(activity).get(OrdersViewModel.class);
+//     getLifecycle().addObserver(viewModel);
+//     LifecycleOwner lifecycleOwner = getViewLifecycleOwner();
+  //   viewModel.getText().observe(lifecycleOwner, this::updateOrdersList);
+
+   }
+
+//  private void updateOrdersList(Li) {
+//
+//  }
+
+  private void updateOrdersList(List<ProductOnOrder> orders) {
+    adapter.getOrders().clear();
+    adapter.getOrders().addAll(orders);
+    adapter.notifyItemInserted(orders.size());
+     //noinspection ConstantConditions
+//     binding.ordersList.getLayoutManager().scrollToPosition(orders.size() - 1);
+  
+   }
 
 }
